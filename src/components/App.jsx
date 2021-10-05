925ae1c60209d7c109651da6368984d481df51ac
An2ans <antoniojesus_92@hotmail.com>
2021-10-05 19:36:51 +0100

    added notes.js with map

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from "./Header.jsx";
+import Note from "./Note.jsx";
+import Footer from "./Footer.jsx";
+import notes from "../notes.js";
+
+
+function App(){
+return (
+  <div>
+  <Header/>
+  {notes.map(notes => (<Note
+    key= {notes.key}
+    title= {notes.title}
+    content= {notes.content}
+    />
+  ))}
+  <Footer/>
+  </div>
+);
+}
+
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+  const year = new Date().getFullYear()
+  return (
+  <footer>
+  <p> Copyright © {year} by Antonio Beltrán</p>
+  </footer>);
+}
+
+export default Footer;
